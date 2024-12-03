e020f4f71b65c5863d06c991ac82241e83025b8a
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-12-03 20:35:21 +0500

    Update the properties of the window
    - Change the parameters for the size of the window
    - Added the new parameter to disable resizing the window

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,10 +6,11 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 720,
     show: false,
     autoHideMenuBar: true,
+    resizable: false,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
