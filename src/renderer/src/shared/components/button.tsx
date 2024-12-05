8dd1388be3d485c184a7d5116e554c6831451248
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-12-05 20:39:03 +0500

    Added a button component

--- src/renderer/src/shared/components/button.tsx
+++ src/renderer/src/shared/components/button.tsx
@@ -1,5 +1,39 @@
-export interface ButtonProps {}
+import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+import { Utils } from '@renderer/utils/utils'
 
-export const Button: React.FC<ButtonProps> = (_props): React.ReactElement => {
-  return <></>
+const buttonVariants = cva(
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  {
+    variants: {
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10'
+      }
+    },
+    defaultVariants: {
+      size: 'default'
+    }
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
+
+const Button: React.FC<ButtonProps> = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, size, asChild = false, ...props }, ref): React.ReactElement => {
+    const Component = asChild ? Slot : 'button'
+    return (
+      <Component className={Utils.cn(buttonVariants({ className, size }))} ref={ref} {...props} />
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
