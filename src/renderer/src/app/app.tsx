d44c0f91007914966b4d4d00b9728f0b56836cf7
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2024-12-05 17:21:01 +0500

    Added custom title bar
    - Updated config of the main window
    - Added basic styles for the title bar
    - Created a div with the id 'titlebar' in the app/app.tsx file

--- src/renderer/src/app/app.tsx
+++ src/renderer/src/app/app.tsx
@@ -2,5 +2,10 @@ import { AppRouting } from '@renderer/views'
 import { withProviders } from './providers'
 
 export const Application: React.FC = withProviders((): React.ReactElement => {
-  return <AppRouting />
+  return (
+    <>
+      <div id="titlebar" />
+      <AppRouting />
+    </>
+  )
 })
